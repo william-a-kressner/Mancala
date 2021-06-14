b449307051c8d81aa968af28f57466cb3f2b6e25
William <william.kressner@gmail.com>
2021-06-13 20:50:08 -0400

    Clean up

--- Mancala.java
+++ Mancala.java
@@ -1,7 +1,6 @@
 public class Mancala {
     public static void main(String[] args) {
         Board board = new Board();
-        // Board board = new Board(Arrays.asList(0,0,0,0,0,1,5, 0,3,4,2,2,3,8));
         board.startGame();
     }
 }
